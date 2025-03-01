d221557869e05b2b2d94e793a0bed9cd1ccd14fc
Aabid <zainulaabid0007@gmail.com>
2025-03-01 14:35:06 +0530

    final

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
-  });
+});
