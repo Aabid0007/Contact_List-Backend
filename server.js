673edb5a2766c25bfd2efbfcae768b943df03717
Aabid <zainulaabid0007@gmail.com>
2025-03-01 13:39:33 +0530

    final

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
     credentials: true,
 }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
